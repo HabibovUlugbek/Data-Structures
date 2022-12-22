dc8a570b29d27a1b1d975dec7cbdbb92c7fc4a90
HabibovUlugbek <habibovulugbek22@gmail.com>
2022-12-22 13:14:43 +0500

    added stack implementation

--- stack.cpp
+++ stack.cpp
@@ -51,17 +51,140 @@ public:
     {
         if (IsEmpty())
         {
-            cout << "there is no element" << endl;
+            cout << "stack underflow" << endl;
+            return 0;
         }
         else
         {
-            return arr[top];
+            int popValue = arr[top];
+            arr[top] = 0;
             top--;
+            return popValue;
+        }
+    }
+
+    int count()
+    {
+        return top + 1;
+    }
+
+    int peek(int pos)
+    {
+        if (IsEmpty())
+        {
+            cout << "stack underflow" << endl;
+            return 0;
+        }
+        else
+        {
+            return arr[pos];
+        }
+    }
+
+    void change(int pos, int val)
+    {
+        if (IsEmpty())
+        {
+            cout << "stack underflow" << endl;
+        }
+        else if (pos > top)
+        {
+            cout << "invalid position" << endl;
+        }
+        else if (pos < 0)
+        {
+            cout << "invalid position" << endl;
+        }
+        else if (pos <= top)
+        {
+            arr[pos] = val;
+            cout << "value changed at location " << pos << endl;
+        }
+    }
+
+    void display()
+    {
+        cout << "All values in the Stack are " << endl;
+        for (int i = 4; i >= 0; i--)
+        {
+            cout << arr[i] << endl;
         }
     }
 };
 
 int main()
 {
+    Stack s1;
+    int option, position, value;
+
+    do
+    {
+        cout << "What operation do you want to perform? Select Option number. Enter 0 to exit." << endl;
+        cout << "1. Push()" << endl;
+        cout << "2. Pop()" << endl;
+        cout << "3. isEmpty()" << endl;
+        cout << "4. isFull()" << endl;
+        cout << "5. peek()" << endl;
+        cout << "6. count()" << endl;
+        cout << "7. change()" << endl;
+        cout << "8. display()" << endl;
+        cout << "9. Clear Screen" << endl
+             << endl;
+
+        cin >> option;
+        switch (option)
+        {
+        case 0:
+            break;
+        case 1:
+            cout << "Enter an item to push in the stack" << endl;
+            cin >> value;
+            s1.push(value);
+            break;
+        case 2:
+            cout << "Pop Function Called - Poped Value: " << s1.pop() << endl;
+            break;
+        case 3:
+            if (s1.IsEmpty())
+                cout << "Stack is Empty" << endl;
+            else
+                cout << "Stack is not Empty" << endl;
+            break;
+        case 4:
+            if (s1.isFull())
+                cout << "Stack is Full" << endl;
+            else
+                cout << "Stack is not Full" << endl;
+            break;
+        case 5:
+            cout << "Enter position of item you want to peek: " << endl;
+            cin >> position;
+            cout << "Peek Function Called - Value at position " << position << " is " << s1.peek(position) << endl;
+            break;
+        case 6:
+            cout << "Count Function Called - Number of Items in the Stack are: " << s1.count() << endl;
+            break;
+        case 7:
+            cout << "Change Function Called - " << endl;
+            cout << "Enter position of item you want to change : ";
+            cin >> position;
+            cout << endl;
+            cout << "Enter value of item you want to change : ";
+            cin >> value;
+            s1.change(position, value);
+            break;
+        case 8:
+            cout << "Display Function Called - " << endl;
+            s1.display();
+            break;
+        case 9:
+            system("cls");
+            break;
+        default:
+            cout << "Enter Proper Option number " << endl;
+        }
+
+    } while (option != 0);
+
     return 0;
 };
